28db0f65c9c6f761de668cbd75e9fa04c1a09d20
Sean <ll14s26s@leeds.ac.uk>
2022-05-10 17:58:50 +0100

    Add Bell state example

--- examples/bell_state.py
+++ examples/bell_state.py
@@ -5,13 +5,12 @@ register = qs.QuantumRegister(3)
 # Initialise |psi> = 1/sqrt(2) (|0> + |1>)
 register.applyGate('H', 1)
 
-# teleportation algorithm
-register.applyGate('H', 2)
-
-register.applyGate('CNOT', 2, 3)
-
+# Entangle qubit 1 & 2
 register.applyGate('CNOT', 1, 2)
 
-register.applyGate('H', 1)
+# This is a Bell state - maximally entangled
+# If qubit 1 is in state |0> then qubit 2
+# is also in state |0> Qubit 1 has 50%
+# probability of being in |0>
 
 print(register.measure())
